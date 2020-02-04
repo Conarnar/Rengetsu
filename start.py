a99f0cb449330381b2e4d370d9f0546c6b3747d9
Conan Truong <truong.conan@gmail.com>
2020-02-03 18:10:43 -0800

    Create and read from settings file

---
+++ start.py
@@ -0,0 +1,29 @@
+import os
+import json
+from pathlib import Path
+
+def start(token):
+	print(token)
+
+if __name__ == '__main__':
+	path = 'reng_dat/'
+	file = path + 'settings.txt'
+	if (os.path.isfile(file)):
+		try:
+			with open(file) as f:
+				settings = json.load(f)
+				start(settings['token'])
+		except OSError:
+			print('Could not read settings file:', str(e))
+	else:
+		try:
+			token = input('Please enter token: ')
+			settings = {'token' : token}
+			if (not os.path.isdir(path)):
+				os.mkdir(path)
+			with open(file, 'w') as f:
+				json.dump(settings, f, indent=4)
+			print('Settings file created:', Path('reng_dat/settings.txt').absolute())
+			start(token)
+		except OSError as e:
+			print('Could not create settings file:', str(e))
