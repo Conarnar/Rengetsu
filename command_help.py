c221f9edaa24349e5b40120feae2a90392780838
Conan Truong <truong.conan@gmail.com>
2020-06-17 20:45:27 -0700

    Fixed error

--- command_help.py
+++ command_help.py
@@ -13,7 +13,7 @@ commands_usages = {
 	'help': {'!help': 'lists all commands', '!help <command>': 'explains the usage of <command>'},
 	'here': {'!here <role>': 'pings everyone with <role> who is online', 'Aliases': '!ping'},
 	'math': {'!math <expression>': 'calculates <expression>', 'Aliases': '!calc'},
-	'mathf': {'!mathf const <name> <value>': 'Using <name> in calculations will replace it with <value>', '!mathf func <name>([args,]) <expression>': 'Creates a custom function'
+	'mathf': {'!mathf const <name> <value>': 'Using <name> in calculations will replace it with <value>', '!mathf func <name>([args,]) <expression>': 'Creates a custom function',
 		'!mathf del <name>': 'Deletes the custom constant/function', '!mathf list': 'Lists your custom constants/functions'},
 	'role': {'!role <role>': 'opens a menu to manage <role>'},
 	'request': {'!request <role>': 'requests to add or remove <role>', 'Aliases': '!requestrole'},
